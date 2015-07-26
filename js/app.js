de4ace6db98a98cb752d9557520969b2382f5189
Ryan Medeiros <ryan.medeiros@thinkcapsule.com>
2015-07-26 15:48:42 -0400

    Update app.js
    Added google sign on (practically the same as facebook)

--- js/app.js
+++ js/app.js
@@ -7,10 +7,26 @@ app.controller("loginCtrl", function($scope, $firebaseObject) {
  		ref.authWithOAuthPopup("facebook", function(error, authData) {
 	  	if (error) {
 	    	console.log("Login Failed!", error);
+	    	alert("That didn't work");
 		  } else {
 		    console.log("Authenticated successfully with payload:", authData);
 		    document.write("You're logged in!");
 		  }
 		});
 	};
+
+	$scope.loginWithGoogle = function() {
+		var ref = new Firebase("https://radiant-inferno-4979.firebaseio.com");
+			ref.authWithOAuthPopup("google", function(error, authData) {
+	  		if (error) {
+	    		console.log("Login Failed!", error);
+	    		alert("That didn't work");
+	  		} 
+	  		else {
+	    		console.log("Authenticated successfully with payload:", authData);
+	    		document.write("You're logged in!");
+	  		}
+		});
+	};
+
 });
