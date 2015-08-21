0220441ab599e5d566cf057a523ba7a4356637ae
Michael <m.ding5692@gmail.com>
2015-08-21 00:37:46 -0400

    Changed to AngularFire Auth format

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 var app = angular.module("loginApp", ["firebase"]);
 
-app.controller("loginCtrl", function($scope, $firebaseObject) {
+app.controller("loginCtrl", function($scope, $firebaseAuth) {
   	var ref = new Firebase("https://radiant-inferno-4979.firebaseio.com");
 
+  	var auth = $firebaseAuth(ref);
+
   	$scope.loginWithFacebook = function() {
- 		ref.authWithOAuthPopup("facebook", function(error, authData) {
-	  	if (error) {
-	    	console.log("Login Failed!", error);
-	    	alert("That didn't work");
-		  } else {
-		    console.log("Authenticated successfully with payload:", authData);
-		    document.write("You're logged in!");
-		  }
-		});
+		  auth.$authWithOAuthPopup("facebook").then(function(authData) {
+		    console.log("Logged in as:", authData.uid);
+		  }).catch(function(error) {
+		    console.log("Authentication failed:", error);
+		  });
 	};
 
 	$scope.loginWithGoogle = function() {
