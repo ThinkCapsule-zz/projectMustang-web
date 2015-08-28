6c27ea0238355107b6531a3391d95b7034b0f0ac
Michael <m.ding5692@gmail.com>
2015-08-28 00:10:34 -0400

    Created basic app skeleton

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*** CONSTRUCTED BY THE THINKCAPSULE WEB TEAM 
+	
+	Created by Michael Ding, Ryan Medeiros and Esther Kim
+
+	Last modified : 08/28/2015
+
+***/
+
+var app = angular.module("kapacinoWebAppApp", ["firebase"]);
---
+++ js/directives.js
@@ -0,0 +1,9 @@
+/*** CONSTRUCTED BY THE THINKCAPSULE WEB TEAM 
+	
+	Created by Michael Ding, Ryan Medeiros and Esther Kim
+
+	- Navbar = A navbar component based on Bootstrap styling with User profile
+
+	Last modified : 08/28/2015
+
+***/
