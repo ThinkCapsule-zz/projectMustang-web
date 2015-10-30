c907e72ea868b8dceb6a92ff7fe036a3ed4a9c60
Michael <m.ding5692@gmail.com>
2015-10-30 14:55:43 -0400

    Configured template to our firebase settings

--- app/config.js
+++ app/config.js
@@ -11,7 +11,7 @@ angular.module('myApp.config', [])
   .constant('loginRedirectPath', '/login')
 
   // your Firebase data URL goes here, no trailing slash
-  .constant('FBURL', 'https://angularfire-seed-dev.firebaseio.com')
+  .constant('FBURL', 'https://radiant-inferno-4979.firebaseio.com')
 
   // double check that the app has been configured before running it and blowing up space and time
   .run(['FBURL', '$timeout', function(FBURL, $timeout) {
